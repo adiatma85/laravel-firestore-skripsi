6145a11ecbbc2c87c6ca82ee303ad309fc894f65
adiatma85 <adiatma85@gmail.com>
2022-10-31 21:39:17 +0700

    crud complete

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,9 +8,9 @@ class CategoriesController extends Controller
 {
     public function index(){
         $data = app('firebase.firestore')
-                    ->database()
-                    ->collection('categories')
-                    ->documents();
+            ->database()
+            ->collection('categories')
+            ->documents();
 
         if ($data->isEmpty()) {
             return collect();
@@ -19,27 +19,53 @@ class CategoriesController extends Controller
         $categoriesRow = collect($data->rows());
         $categories = [];
         foreach ($categoriesRow as $cat) {
+            // return response()->json($cat->data());
             $category = [
+                'id' => $cat->id(),
                 'name' => $cat->data()['name'],
             ];
             array_push($categories, $category);
         }
         return response()->json($categories);
-        // return view('categories.index', [
-        //     'categories' => $categories,
-        // ]);
     }
 
     public function store(Request $request)
     {
-    $firestore = app('firebase.firestore')
-                    ->database()
-                    ->collection('categories')
-                    ->newDocument();
-    $data = [
-        'name' => "Tragedy"
-    ];
-    $firestore->set($data);
-    // Redirect, Success message...
+        $firestore = app('firebase.firestore')
+            ->database()
+            ->collection('categories')
+            ->newDocument();
+        $data = [
+            'name' => $request->post('name'),
+        ];
+        $firestore->set($data);
+        // Redirect, Success message...
+
+        // return response()->json();
+    }
+
+    public function edit(Request $request, $id){
+        $firestore = app('firebase.firestore')
+            ->database()
+            ->collection('categories')
+            ->document($id);
+        
+        $data = [
+            'name' => $request->post('name'),
+        ];
+
+        $firestore->set($data);
+
+        // Return the appropriate response
+    }
+
+    public function delete($id){
+        $firestore = app('firebase.firestore')
+            ->database()
+            ->collection('categories')
+            ->document($id)
+            ->delete();
+
+        // Return the appropriate response
     }
 }
